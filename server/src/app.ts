a7bf3ea05b4ea81e037ad367d15b32ccdd1cd02e
priyans619 <priyansh.2b@gmail.com>
2025-08-28 01:24:22 +0530

    add: morgan logger middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import healthRoutes from "./routes/health";
+import morgan from "morgan";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(morgan("dev")); 
 
 // Routes
 app.use("/health", healthRoutes);
 
 export default app;
-
