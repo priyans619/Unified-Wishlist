5451f1390dfc054f35f2aae8a64b7e627a301b02
priyans619 <priyansh.2b@gmail.com>
2025-08-28 00:17:47 +0530

    -rm

--- server/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from "express";
-
-const router = Router();
-
-router.get("/hello", (req: Request, res: Response) => {
-  res.json({ message: "Hello from CommonJS Express + TS!" });
-});
-
-export default router;
