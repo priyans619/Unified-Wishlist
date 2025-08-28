a4f1211571450c2fd979c7e8e49bc344e650b145
priyans619 <priyansh.2b@gmail.com>
2025-08-28 16:27:38 +0530

    update: app.ts import route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,17 @@
+// app.ts
 import express from "express";
-import healthRoutes from "./routes/health";
 import morgan from "morgan";
+import healthRoutes from "./routes/health";
+import previewRoutes from "./routes/preview";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(morgan("dev")); 
+app.use(morgan("dev"));
 
 // Routes
 app.use("/health", healthRoutes);
+app.use("/preview", previewRoutes);
 
 export default app;
